f32b452830231645760739b56f25d29a0dae68c5
ilyass atlassi <atlassiilyass@gmail.com>
2023-05-26 00:53:30 +0100

    Add fizzbuzz

--- solver.rb
+++ solver.rb
@@ -11,5 +11,12 @@ class Solver
     def reverse(word)
         word.reverse
       end
+      
+      def fizzbuzz(n)
+        return 'fizzbuzz' if (n % 3).zero? && (n % 5).zero?
+        return 'fizz' if (n % 3).zero?
+        return 'buzz' if (n % 5).zero?
+        n.to_s
+      end
   end
   
