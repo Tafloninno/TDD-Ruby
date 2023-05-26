fbb583e1621f04d062627b1d6e85cbf577e19c40
Mugisha Innocent <111200646+Tafloninno@users.noreply.github.com>
2023-05-26 11:03:52 +0300

    create test for factorial method

---
+++ spec/solver_spec.rb
@@ -0,0 +1,25 @@
+require_relative '../solver'
+
+describe Solver do
+  let(:solver) { Solver.new }
+
+  describe '#factorial' do
+    it 'returns 1 when given 0' do
+      expect(solver.factorial(0)).to eq(1)
+    end
+
+    it 'returns the correct factorial for a positive number' do
+      expect(solver.factorial(5)).to eq(120)
+      expect(solver.factorial(10)).to eq(3_628_800)
+    end
+
+    it 'raises an exception for negative numbers' do
+      expect { solver.factorial(-5) }.to raise_error(ArgumentError, 'Factorial input must be a non-negative integer')
+    end
+
+    it 'raises an exception for non-integer inputs' do
+      expect { solver.factorial(3.5) }.to raise_error(ArgumentError, 'Factorial input must be a non-negative integer')
+      expect { solver.factorial('abc') }.to raise_error(ArgumentError, 'Factorial input must be a non-negative integer')
+    end
+  end
+end
